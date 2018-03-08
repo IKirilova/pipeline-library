ae274e7705249caa8471b5dd80b2b9abe13925b6
Irina Kirilova <irina.kirilova@sap.com>
2018-03-08 09:41:56 +0100

    Update generateMtaDescriptor.groovy

--- vars/generateMtaDescriptor.groovy
+++ vars/generateMtaDescriptor.groovy
@@ -7,11 +7,11 @@ def call(parameters = [:]) {
   handlePipelineStepErrors (stepName: stepName, stepParameters: parameters) {
       
       def data = readJSON file: parameters.srcFile
-      appName = params.applicationName
+     
       mtaData  = readYaml text: libraryResource('mta.yml')
       mtaData['ID'] = data.name
       mtaData['version'] = data.version
-      mtaData['modules'][0]['name'] = appName
+      mtaData['modules'][0]['name'] = parameters.applicationName
       mtaData['modules'][0]['parameters']['version'] = "${data.version}-\${timestamp}"
 
       writeYaml file: parameters.targetFile, data: mtaData
